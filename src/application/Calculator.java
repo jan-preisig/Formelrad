f119685c2464af309c4fb22d74fc935e8ef58bd0
jan_preisig <jan_preisig@gmx.net>
2018-10-02 12:18:03 +0200

    add method to calculate p

--- src/application/Calculator.java
+++ src/application/Calculator.java
@@ -2,54 +2,66 @@ package application;
 
 /**
  * Berechnet das Formelrad
+ *
  * @author Peter Rutschmann
  * @version 13.09.2018
  */
 public class Calculator {
-	private double leistung;
-	private double spannung;
-	private double strom;
-	private double widerstand;
-	
-	public Calculator(double leistung, double spannung, double strom, double widerstand) {
-		super();
-		this.leistung = leistung;
-		this.spannung = spannung;
-		this.strom = strom;
-		this.widerstand = widerstand;
-	}
-	
-	public double getLeistung() {
-		return leistung;
-	}
-	
-	public double getSpannung() {
-		return spannung;
-	}
 
-	public double getStrom() {
-		return strom;
-	}
+  private final double leistung;
+  private final double spannung;
+  private final double strom;
+  private final double widerstand;
 
-	public double getWiderstand() {
-		return widerstand;
-	}
+  public Calculator(double leistung, double spannung, double strom, double widerstand) {
+    super();
+    this.leistung = leistung;
+    this.spannung = spannung;
+    this.strom = strom;
+    this.widerstand = widerstand;
+  }
 
-	@Override
-	public String toString() {
-		return "Calculator [leistung=" + leistung + 
-				", spannung=" + spannung + 
-				", strom=" + strom + 
-				", widerstand="	+ widerstand + "]";
-	}
+  public double pAusUundI(double u, double i) {
+    return u * i;
+  }
 
-	public void calculate() {
-		/* Hier auf Grund der vorhanden Werte entscheiden
-		 * welche Methode unten aufgerufen werden muss.
-		 */
-	}
-	
-	/* Hier die Methoden mit den Formlen hinzufügen
-	 */
-	
+  public double pAusUundR(double u, double r) {
+    return (u * u) / r;
+  }
+
+  public double pAusRundI(double r, double i) {
+    return r * (i * i);
+  }
+
+  public double getLeistung() {
+    return leistung;
+  }
+
+  public double getSpannung() {
+    return spannung;
+  }
+
+  public double getStrom() {
+    return strom;
+  }
+
+  public double getWiderstand() {
+    return widerstand;
+  }
+
+  @Override
+  public String toString() {
+    return "Calculator [leistung=" + leistung + ", spannung=" + spannung + ", strom=" + strom + ", widerstand="
+        + widerstand + "]";
+  }
+
+  public void calculate() {
+    /* Hier auf Grund der vorhanden Werte entscheiden
+     * welche Methode unten aufgerufen werden muss.
+     */
+  }
+
+  /* Hier die Methoden mit den Formlen hinzufï¿½gen
+   */
+
 }
