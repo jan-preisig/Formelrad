6dbe0dda4be6a4dee248a986fe02c60380219333
jan_preisig <jan_preisig@gmx.net>
2018-10-02 12:28:09 +0200

    implement formula to calculate i from P and U

--- src/application/Calculator.java
+++ src/application/Calculator.java
@@ -37,6 +37,9 @@ public class Calculator {
     return Math.sqrt(p / r);
   }
 
+  public double iAusPundU(double p, double u){
+    return p/u;
+  }
 
   public double getLeistung() {
     return leistung;
